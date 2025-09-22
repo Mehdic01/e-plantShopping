5d9195a1a61eba17f0c1f6638b493de5d06ba736
Mehdi <mehdinov16@gmail.com>
2025-09-22 14:12:03 +0300

    redux

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,57 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0
+
+    cart.forEach(item => {
+      const quantity = item.quantity || 1; // Default to 1 if not set
+      const cost = parseFloat(item.cost.substring(1)); // remove "$" and convert to number
+      total += cost * quantity;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+   e.preventDefault();
+   onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({
+      name: item.name,
+      quantity: item.quantity + 1
+    }));
+
   };
 
   const handleDecrement = (item) => {
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({
+        name: item.name,
+        quantity: item.quantity - 1
+      }));
+    } else {
+      dispatch(removeItem(item.name));
+    }
    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const cost = parseFloat(item.cost.substring(1));
+    const totalCost = item.quantity * cost;
+    return totalCost;
   };
+
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
   return (
     <div className="cart-container">
@@ -64,5 +93,4 @@ const CartItem = ({ onContinueShopping }) => {
 };
 
 export default CartItem;
-
 
